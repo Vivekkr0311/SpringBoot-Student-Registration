6ed019a70313cbfe9a895e0a7647331dea85d0d1
Vivek Kumar <vivekkr0311@gmail.com>
2023-11-07 14:43:19 +0530

    Show all student page add

--- src/main/java/com/StudentRegistration/StudentRegistration/Controllers/MainController.java
+++ src/main/java/com/StudentRegistration/StudentRegistration/Controllers/MainController.java
@@ -7,6 +7,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 public class MainController {
 
@@ -26,11 +30,22 @@ public class MainController {
         return "student-registration";
     }
 
-    @GetMapping(path = "/all")
-    public @ResponseBody Iterable<Student> getAllStudents(@ModelAttribute Student student){
+//    @GetMapping(path = "/all")
+//    public @ResponseBody Iterable<Student> getAllStudents(@ModelAttribute Student student){
+//        for(Student it : studentRepository.findAll()){
+//            System.out.println(it.getSname() + " " + it.getRollno());
+//        }
+//        return studentRepository.findAll();
+//    }
+
+    @GetMapping("/all-student.html")
+    public String list(Model model){
+        List<String> temp = new ArrayList<>();
         for(Student it : studentRepository.findAll()){
-            System.out.println(it.getSname() + " " + it.getRollno());
+            temp.add(it.getSname() + " " + it.getRollno());
         }
-        return studentRepository.findAll();
+
+        model.addAttribute("allStudent", Arrays.asList(temp));
+        return "all-student";
     }
 }
